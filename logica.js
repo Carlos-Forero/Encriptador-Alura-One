d5005cc6096aec98ca1e250b033530d05ffa0dc1
Carlos Forero <caforero93@gmail.com>
2023-01-13 17:16:56 -0500

    eliminado (encripter2) archivo inecesario y limpieza en codigo

--- logica.js
+++ logica.js
@@ -16,16 +16,12 @@ function encriptar(){
         document.getElementById('textoSalida').value = '';
     }
     if (tamano2>0){
-        //document.write("hay texto")
-        /* let t=document.getElementById('ingresaTexto') */
      
-        
         if (/[A-Z]/g.test(t)||/[áéíóúÁÉÍÓÚ]/g.test(t)){
             console.log("A incertado mayusculas");
             alert("Recuerde solo ingresar texto en minuscula y sin acentos");
         }else{
 
-       
         //t=t.replace(/\n/g, "<br>");
         var letra=t.split("");
         console.log(tamano);
@@ -74,22 +70,17 @@ function desencriptar(){
         document.getElementById('textoSalida').value = '';
     }
     if (tamano2>0){
-        //document.write("hay texto")
-        /* let t=document.getElementById('ingresaTexto') */
-        
     
         if (/[A-Z]/g.test(t)||/[áéíóúÁÉÍÓÚ]/g.test(t)){
             console.log("A incertado mayusculas");
             alert("Recuerde solo ingresar texto en minuscula y sin acentos");
         }else{
 
-       
        // document.getElementById('ingresaTexto').textContent = t.replace(/\n/g, "<br>")
        // t=t.replace(/\n/g, "<br>");
         var nt= t.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
         //var posicion=t.indexOf("enter");
-       // 
-        
+    
         /* while (posicion>=0)
         {
             t=t.slice(0,posicion)+"e"+t.slice(posicion+5);
@@ -102,30 +93,13 @@ function desencriptar(){
     }
 }
 function copiar(Copia){
-      // Crea un campo de texto "oculto", este por un textarea
 
   var aux = document.createElement("textarea");
-/*
-    var t=document.getElementById('textoSalida').value;
-    t=t.replace(/<br>/g, "\n");
-    document.getElementById('textoSalida').innerHTML=t;
-*/
-
-  // Asigna el contenido del elemento especificado al valor del campo
-  // este para vaciar el contenido
 
   aux.innerHTML = document.getElementById('textoSalida').value
-
-  // Añade el campo a la página
   document.body.appendChild(aux);
-
-  // Selecciona el contenido del campo
   aux.select();
-
-  // Copia el texto seleccionado
   document.execCommand("copy");
-
-  // Elimina el campo de la página
   document.body.removeChild(aux);
 
 
@@ -156,13 +130,6 @@ function ocultarMuneco(){
     }
     
 }
-/*
-if(window.screen.availWidth == 500){
-    document.getElementById('ingresaTexto').style.background ='pink';
-    console.log("esta en zona");
-}
-*/
-
 
 
 /* REGLAS:
